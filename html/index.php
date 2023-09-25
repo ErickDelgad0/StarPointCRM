d7ef0a5205015ecf72d932de215d21c25200373a
Erick Delgado <edelgado24@wooster.edu>
2023-09-25 11:22:34 -0400

    Next Setup to continue development

---
+++ html/index.php
@@ -0,0 +1,24 @@
+<!-- localhost:8080/html/index.php -->
+
+<?php include '../php/CustomerFunctions.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StarPoint</title>
+</head>
+<body>
+    
+    <h1> Customer Information </h1>
+
+    <div>
+        <?php
+        echo DisplayCustomerTable();
+        ?>
+    </div>
+
+</body>
+</html>
---
+++ php/CustomerFunctions.php
@@ -0,0 +1,47 @@
+<!-- http://localhost:8080/index.php -->
+
+<?php
+
+function DisplayCustomerTable() {
+    $dbconfig = parse_ini_file("../.env");
+
+    $db_connect = mysqli_connect(
+        'db', # db service name
+        $dbconfig["MYSQL_USER"], # username
+        $dbconfig["MYSQL_PASSWORD"], # password
+        $dbconfig["MYSQL_DATABASE"] # database name
+    );
+
+    $Customer_Table = "Customer";
+
+    $Query_Customer_Table = "SELECT * FROM $Customer_Table";
+
+    $Response_Customer_Table = mysqli_query($db_connect, $Query_Customer_Table);
+
+    $tableHTML = "<table class='customer-table'>";
+    $tableHTML .= "<thead>";
+    $tableHTML .= "<tr>";
+    $tableHTML .= "<th>ID</th>";
+    $tableHTML .= "<th>First Name</th>";
+    $tableHTML .= "<th>Last Name</th>";
+    $tableHTML .= "<th>DOB</th>";
+    $tableHTML .= "</tr>";
+    $tableHTML .= "</thead>";
+
+    $tableHTML .= "<tbody>";
+    while($i = mysqli_fetch_assoc($Response_Customer_Table)) {
+        $tableHTML .= "<tr>";
+        $tableHTML .= "<td>".$i['id']."</td>";
+        $tableHTML .= "<td>".$i['firstname']."</td>";
+        $tableHTML .= "<td>".$i['lastname']."</td>";
+        $tableHTML .= "<td>".$i['DOB']."</td>";
+        $tableHTML .= "</tr>";
+    }
+    $tableHTML .= "</tbody>";
+
+    $tableHTML .= "</table>";
+
+    return $tableHTML;
+}
+
+?>
