ab8dfd3ed7a5553a858a1272b6b4597f8966ac6e
Erick Delgado <edelgado24@wooster.edu>
2023-11-13 11:16:13 -0500

    changed the logo

--- php/functions.php
+++ php/functions.php
@@ -105,7 +105,7 @@ function CRM_header($title) {
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                    <i class="fas"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">Myaca</div>
             </a>
